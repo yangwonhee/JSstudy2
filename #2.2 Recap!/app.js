d4e880b1dc45576cd5f19c3a8144d72b6dc22648
yangwonhee <yangwonhee@kakao.com>
2022-01-30 17:51:36 +0900

    #2.2 Recap

--- #2.2 Recap!/app.js
+++ #2.2 Recap!/app.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d")
 
+canvas.width = 700;
+canvas.height = 700;
+
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
@@ -34,3 +37,5 @@ if(canvas){
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+// https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
